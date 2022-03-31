ab9f7ddb7ef88c21e9cf93e527570df903076397
Strexer <98621688+Strexer@users.noreply.github.com>
2022-03-31 16:43:53 +0300

    bitch spoofer
    name:nigga 
    pass:cracked

---
+++ message_1.cmd
@@ -0,0 +1,345 @@
+��@echo off
+tile loading please wait
+color 5
+set load=
+set/a loadnum=
+
+:loading
+set load=%load%Û
+cls
+echo.
+echo Wait To Load
+echo -------------------------------------
+echo %load%
+echo -------------------------------------
+ping localhost -n 2 >nul
+set/a loadnum=%loadnum% +1
+if %loadnum%==2 goto done
+
+goto loading
+:done
+echo.
+echo loading complete
+
+:login
+cls
+color 5
+
+chcp 65001
+color 5                                                
+echo                                                                                                                []═══════════════════════════════════════════════[]        
+echo                                                                                                                [] if you have amy problem contact with support  []
+echo                                                                                                                []═══════════════════════════════════════════════[]
+echo                                                                                                                             []══════════════════════════════[]
+echo                                                                                                                             [] share account = blacklist key[]   
+echo                                                                                                                             []══════════════════════════════[]
+echo                                                                  []═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════[]
+echo                                                                  [] login to spoofer wait 15 Senconds after closed spoofer open fivem restart your router and login with new rockstar account []
+echo                                                                  []═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════[]
+
+echo     Bitchspoofer
+echo     ¶¶¶¢¢¢¢¶¶¶
+echo    1¶¶¢¢¢¢¢¢¶¶                                               
+echo    ¶¶¢¢¢¢¢¢¢¶1
+echo    ¶¢¢¢¢¢¢¢¢¶¶
+echo   ¶¶¢¢¢¢¢¢¢¢¢¶     
+echo   1¶¢¢¢¢¢¢¢¢¢1   
+echo  7¶¢¢¢¢¢¢¢¢¢¶¶¶     
+echo   ¶¢¢¢¢¢¢¢¢¢¢¢¶¶    
+echo  ¶¶¶¶¶¶¢¢¢¢¢¢¢¶    ▄▄▄▄    ██▓▄▄▄███
+echo 1¶¶¶  ¶¶¢¢¢¢¢¶¶    ▓█████▄ ▓██▒▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒            
+echo ¶¶¶    ¶¶¢¢¢¢¶7    ▒██▒ ▄██▒██▒▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░
+echo 7¶¢¢1   ¶¢¢¢¢¶     ▒██░█▀  ░██░░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ 
+echo ¶¶¶¶¶¶¶¢¢¢¢1¶¢     ░▓█  ▀█▓░██░  ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓   
+echo    1¢¢¢¢¢¢¶7       ░▒▓███▀▒░▓    ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒  
+echo   ¢¢¢1111¢¶
+echo   ¶¶¢¢1111¶¶
+echo    1¶¢111111¶
+echo    ¶¢111111¶
+echo    ¶¢111111¶
+
+
+
+
+
+
+
+
+
+
+
+set /p user=Username :                                                  
+echo.                                                                                                                            
+set /p pass=Password :                           
+if %user% == 570705088809 if %pass% == 2985725958781 goto main
+echo Error 07, Sorry! It Appears You Have Did The Wrong Password, Or Username Wait 5 Senconds
+timeout 5 >nul
+goto login
+an exeis spasi to crypto palio skiddera pare kanena tsimpouki iparxoun ala 15 nea version k auto to spoofer vgazi ban mono apo 4 server hawk auto to spoofer einai made by prezas okkkk
+:main
+cls
+echo welcome %bitch%
+cls
+title *bitch-spoofer
+color 5
+color 5
+echo     Bitchspoofer
+echo     ¶¶¶¢¢¢¢¶¶¶
+echo    1¶¶¢¢¢¢¢¢¶¶                                               
+echo    ¶¶¢¢¢¢¢¢¢¶1
+echo    ¶¢¢¢¢¢¢¢¢¶¶
+echo   ¶¶¢¢¢¢¢¢¢¢¢¶     
+echo   1¶¢¢¢¢¢¢¢¢¢1   
+echo  7¶¢¢¢¢¢¢¢¢¢¶¶¶     
+echo   ¶¢¢¢¢¢¢¢¢¢¢¢¶¶    
+echo  ¶¶¶¶¶¶¢¢¢¢¢¢¢¶    ▄▄▄▄    ██▓▄▄▄███
+echo 1¶¶¶  ¶¶¢¢¢¢¢¶¶    ▓█████▄ ▓██▒▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒            
+echo ¶¶¶    ¶¶¢¢¢¢¶7    ▒██▒ ▄██▒██▒▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░
+echo 7¶¢¢1   ¶¢¢¢¢¶     ▒██░█▀  ░██░░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ 
+echo ¶¶¶¶¶¶¶¢¢¢¢1¶¢     ░▓█  ▀█▓░██░  ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓   
+echo    1¢¢¢¢¢¢¶7       ░▒▓███▀▒░▓    ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒  
+echo   ¢¢¢1111¢¶
+echo   ¶¶¢¢1111¶¶
+echo    1¶¢111111¶
+echo    ¶¢111111¶
+echo    ¶¢111111¶                                   
+
+
+echo                                                                                             press 1 to spoof fivem 
+echo                                                                                             ═══════════════════════       
+                                                                           
+set /p key=for spoof press=1: 
+if %key%==1 goto poutanasgios                               
+echo.
+echo.                                                   
+
+:poutanasgios 
+:FIXDISCORD
+color 5
+cls
+RENAME %userprofile%\AppData\Roaming\discord\0.0.309\modules\STARCHARMS_SPOOFER discord_rpc
+taskkill /f /im Discord.exe /t
+timeout 1 >nul
+cls
+echo.
+echo.
+echo                                                       Wait 5 seconds to spoofer closed
+timeout 2 >nul
+:CLEAN/SPOOF
+cls
+taskkill /f /im Steam.exe /t
+set hostspath=%windir%\System32\drivers\etc\hosts
+echo 127.0.0.1 xboxlive.com >> %hostspath%
+echo 127.0.0.1 user.auth.xboxlive.com >> %hostspath%
+echo 127.0.0.1 presence-heartbeat.xboxlive.com >> %hostspath%
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSLicensing\HardwareID /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSLicensing\Store /f
+REG DELETE HKEY_CURRENT_USER\Software\WinRAR\ArcHistory /f
+REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-1282084573-1681065996-3115981261-1001 /va /f
+REG DELETEH KEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\ShowJumpView /f
+REG DELETEH KEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\WinRAR\ArcHistory /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched /f
+REG DELETE HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\ShowJumpView /f
+REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-332004695-2829936588-140372829-1002 /f
+REG DELETE HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched /f
+REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-1282084573-1681065996-3115981261-1001 /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched /f
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cfx_curl_x86_64.dll
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\Browser"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\db"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\dunno"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\priv"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\servers"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\subprocess"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\unconfirmed"
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\steam_api64.dll
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\authbrowser"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\crashometry"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\launcher_skip"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\launcher_skip_mtl2"
+rmdir /s /q "%LocalAppData%\DigitalEntitlements"
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\profiles.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_chrome.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_372.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_1604.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_1868.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_2060.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_2189.bin
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\mods\*.*"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\logs\*.*"
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenGame.dll
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cfx_curl_x86_64.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\steam.dll
+rmdir /s /q %userprofile%\AppData\Roaming\CitizenFX
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\asi-five.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX.ini
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\caches.XML
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\adhesive.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\discord.dll
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\crashes\*.*"
+RENAME %userprofile%\AppData\Roaming\discord\0.0.309\modules\discord_rpc STARCHARMS_SPOOFER
+timeout 1 >nul
+cls
+echo.
+echo.
+echo                                                           bitch love you 
+cls
+taskkill /f /im Steam.exe /t
+rmdir /s /q "%LocalAppData%\DigitalEntitlements"
+cls
+echo.
+echo.
+echo                                                             wait 2 seconds to spoofer automatic closed
+timeout 2 >nul
+goto SPOOFER
+timeout 2 >nul:CITIZENFX
+cls
+taskkill /f /im Steam.exe /t
+rmdir /s /q %userprofile%\AppData\Roaming\CitizenFX
+cls
+echo.
+echo.
+echo                                                             i love you bitch
+timeout 2 >nul
+goto SPOOFER:SPOOFE
+cls
+taskkill /f /im Steam.exe /t
+set hostspath=%windir%\System32\drivers\etc\hosts
+echo 127.0.0.1 xboxlive.com >> %hostspath%
+echo 127.0.0.1 user.auth.xboxlive.com >> %hostspath%
+echo 127.0.0.1 presence-heartbeat.xboxlive.com >> %hostspath%
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSLicensing\HardwareID /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSLicensing\Store /f
+REG DELETE HKEY_CURRENT_USER\Software\WinRAR\ArcHistory /f
+REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-1282084573-1681065996-3115981261-1001 /va /f
+REG DELETEH KEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\ShowJumpView /f
+REG DELETEH KEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\WinRAR\ArcHistory /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched /f
+REG DELETE HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\ShowJumpView /f
+REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-332004695-2829936588-140372829-1002 /f
+REG DELETE HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched /f
+REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-1282084573-1681065996-3115981261-1001 /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched /f
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_chrome.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_372.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_1604.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_1868.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_2060.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_2189.bin
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\botan.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\asi-five.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\steam.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\steam_api64.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenGame.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\profiles.dll
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cfx_curl_x86_64.dll
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX.ini
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\caches.XML
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\adhesive.dll
+cls
+echo.
+echo.
+echo                                                             bitch>3
+timeout 2 >nul
+goto SPOOFER:DISCORD
+cls
+taskkill /f /im Steam.exe /t
+del /s /q /f %LocalAppData%\FiveM\FiveM.app\discord.dll
+RENAME %userprofile%\AppData\Roaming\discord\0.0.309\modules\discord_rpc STARCHARMS_SPOOFER
+cls
+echo.
+echo.
+echo                                                             
+timeout 2 >nul                                                  bitch suck my d$$$
+goto SPOOFER:CACHE
+cls
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\Browser"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\db"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\dunno"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\priv"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\servers"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\subprocess"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\unconfirmed"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\authbrowser"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\crashometry"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\launcher_skip"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\cache\launcher_skip_mtl2"
+cls
+echo.
+echo.
+echo                                                             bitch love prezas
+timeout 2 >nul
+goto CLEAN:CRASH
+cls
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\crashes\*.*"
+cls
+echo.
+echo.
+echo                                                              bitch look wow cute are you
+timeout 2 >nul
+goto CLEAN
+:LOGS
+cls
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\logs\*.*"
+cls
+echo.
+echo.
+echo                                                             Bitch please
+timeout 2 >nul
+goto CLEAN
+:MODS
+cls
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\mods\*.*"
+cls
+echo.
+echo.
+echo                                                             Bitch i can take photo with you 
+timeout 2 >nul
+goto CLEAN
+:main
+cls
+title FiveM Server server
+color 5
+echo Wait to clear all data 
+timeout /t 5 /nobreak > NUL
+rmdir /Q /S %appdata%\CitizenFX >nul 2>&1
+rmdir /Q /S %localappdata%\DigitalEntitlements >nul 2>&1
+rmdir /Q /S %localappdata%\FiveM\FiveM.app\logs >nul 2>&1
+rmdir /Q /S %localappdata%\FiveM\FiveM.app\crashes >nul 2>&1
+del %localappdata%\FiveM\FiveM.app\CitizenFX.ini >nul 2>&1
+del %localappdata%\FiveM\FiveM.app\asi-five.dll >nul 2>&1
+@echo:
+echo just wait 4 seconds 
+@echo:
+timeout /t 3 /nobreak > NUL
+echo closed spoofer and open fivem 
+pause > nul
+start %localappdata%\FiveM\FiveM.ex 
